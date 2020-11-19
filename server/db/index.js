31147f208a0ff6b654d81b26e13fd98587b7f69c
ji21 <jeffreyip54@gmail.com>
2020-11-19 07:54:49 +0000

    created mysql schema

--- server/db/index.js
+++ server/db/index.js
@@ -8,15 +8,35 @@ const connection = mysql.createConnection({
     password: process.env.db_password,
     database: 'sys'
   })
-  
+
+const createProfilesQuery = "CREATE TABLE IF NOT EXISTS Profiles(pid INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(20), age INT NOT NULL, gender VARCHAR(20))"
+const createUsersQuery = "CREATE TABLE IF NOT EXISTS Users(uid INT AUTO_INCREMENT PRIMARY KEY, username VARCHAR(20), password VARCHAR(255) NOT NULL, email VARCHAR(100), phone VARCHAR(50), pid INT, FOREIGN KEY (pid) REFERENCES Profiles(pid))"
+
+
 connection.connect(function(error) {
     if (error) {
-    console.log(error);
+        console.log(error);
     } else {
         console.log("connected to mysql...");
     }
 })
 
+
+connection.query(createProfilesQuery, function(error) {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log("profiles table created");
+    }
+})
+
+connection.query(createUsersQuery, function(error) {
+    if (error) {
+        // console.log(error);
+    } else {
+        console.log("users table created");
+    }
+})
 
 module.exports = connection;
 
