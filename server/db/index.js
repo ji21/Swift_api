d812697ace1342fcdca9b9872530faf10772f990
ji21 <jeffreyip54@gmail.com>
2020-11-19 16:16:13 +0000

    collapsed query into single line

--- server/db/index.js
+++ server/db/index.js
@@ -14,11 +14,9 @@ var dbName = "swiftapi"
 
 const createDatabase = `CREATE DATABASE IF NOT EXISTS ${dbName}`;
 
-const createProfilesQuery = "CREATE TABLE IF NOT EXISTS PROFILES (pid INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(20),\
-                            age INT NOT NULL, gender VARCHAR(20))"
-const createUsersQuery = "CREATE TABLE IF NOT EXISTS USERS (uid INT AUTO_INCREMENT PRIMARY KEY, username VARCHAR(20),\
-                        password VARCHAR(255) NOT NULL, email VARCHAR(100), phone VARCHAR(50), pid INT, \
-                        FOREIGN KEY (pid) REFERENCES PROFILES(pid))"
+const createProfilesQuery = "CREATE TABLE IF NOT EXISTS PROFILES (pid INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(20), age INT NOT NULL, gender VARCHAR(20))"
+
+const createUsersQuery = "CREATE TABLE IF NOT EXISTS USERS (uid INT AUTO_INCREMENT PRIMARY KEY, username VARCHAR(20), password VARCHAR(255) NOT NULL, email VARCHAR(100), phone VARCHAR(50), pid INT, FOREIGN KEY (pid) REFERENCES PROFILES(pid))"
 
 connection.query(createDatabase, function(error) {
     if (error) {
