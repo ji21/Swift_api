46518f4439044082c52d8295b372c3809786b469
ji21 <jeffreyip54@gmail.com>
2020-11-19 09:44:01 +0000

    implemented create and get user

--- server/db/index.js
+++ server/db/index.js
@@ -9,8 +9,11 @@ const connection = mysql.createConnection({
     database: 'sys'
   })
 
-const createProfilesQuery = "CREATE TABLE IF NOT EXISTS Profiles(pid INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(20), age INT NOT NULL, gender VARCHAR(20))"
-const createUsersQuery = "CREATE TABLE IF NOT EXISTS Users(uid INT AUTO_INCREMENT PRIMARY KEY, username VARCHAR(20), password VARCHAR(255) NOT NULL, email VARCHAR(100), phone VARCHAR(50), pid INT, FOREIGN KEY (pid) REFERENCES Profiles(pid))"
+const createProfilesQuery = "CREATE TABLE IF NOT EXISTS Profiles(pid INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(20),\
+                            age INT NOT NULL, gender VARCHAR(20))"
+const createUsersQuery = "CREATE TABLE IF NOT EXISTS Users(uid INT AUTO_INCREMENT PRIMARY KEY, username VARCHAR(20),\
+                        password VARCHAR(255) NOT NULL, email VARCHAR(100), phone VARCHAR(50), pid INT, \
+                        FOREIGN KEY (pid) REFERENCES Profiles(pid))"
 
 
 connection.connect(function(error) {
@@ -38,5 +41,63 @@ connection.query(createUsersQuery, function(error) {
     }
 })
 
-module.exports = connection;
+let db = {users: {all: null, getUserById: null}, profiles: {all: null}};
+
+db.users.all = () => {
+    return new Promise((resolve,reject)=>{
+        connection.query("SELECT * from USERS", (error, results) => {
+            if (error) {
+                return reject(error);
+            }
+            return resolve(results);
+        })
+    })
+}
+
+db.users.getUserById = (id) => {
+    return new Promise ((resolve, reject)=>{
+        connection.query(`SELECT * from USERS WHERE uid = ${id}`, (error, results)=>{
+            if (error) {
+                return reject(error);
+            }
+            return resolve(results);
+        })
+    })
+}
+
+db.users.create = (params) => {
+    if (params.phone == undefined) {
+        return new Promise((resolve, reject) => {
+            connection.query(`INSERT INTO USERS(username, password, email) VALUES ('${params.username}', '${params.password}', '${params.email}')`, (error, results) => {
+                if (error) {
+                    return reject(error);
+                }
+                return resolve(results);
+            })
+        })
+    } else {
+        return new Promise((resolve, reject) => {
+            connection.query(`INSERT INTO USERS(username, password, phone) VALUES ('${params.username}', '${params.password}', '${params.phone}')`, (error, results) => {
+                if (error) {
+                    return reject(error);
+                }
+                return resolve(results);
+            })
+        })
+    }
+}
+
+db.profiles.all = () => {
+    return new Promise((resolve,reject)=>{
+        connection.query("SELECT * from PROFILES", (error, results) => {
+            if (error) {
+                return reject(error);
+            }
+            return resolve(results);
+        })
+    })
+}
+
+
+module.exports = db;
 
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,39 @@
+const { response } = require('express');
 const express = require('express');
 const db = require('../db');
 const router = express.Router();
 
-router.get('/', (req,res,next)=> {
-    res.json({test: 'test'})
+router.get('/users', async (req,res,next)=> {
+    try {
+        let results = await db.users.all();
+        res.json(results);
+    } catch(error) {
+        console.log(error);
+        response.sendStatus(500);
+    }
 })
+
+router.get('/users/:id', async (req,res,next)=>{
+    const id = req.params.id;
+    try {
+        let results =  await db.users.getUserById(id);
+        res.json(results);
+    } catch (error) {
+        console.log(error);
+        response.sendStatus(500);
+    }
+})
+
+router.post('/users', async (req,res) => {
+    const params = req.body;
+    try {
+        let results = await db.users.create(params);
+        res.json(results);
+    } catch (error) {
+        console.log(error);
+        response.sendStatus(500);
+    }
+})
+
 
 module.exports = router;
