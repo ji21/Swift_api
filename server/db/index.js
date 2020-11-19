e2388fa9494a29f533e56d2da70442aa98f3e3b3
ji21 <jeffreyip54@gmail.com>
2020-11-19 16:06:54 +0000

    log full user create error

--- server/db/index.js
+++ server/db/index.js
@@ -52,6 +52,7 @@ connection.query(createProfilesQuery, function(error) {
 connection.query(createUsersQuery, function(error) {
     if (error) {
         console.log("users table failed to create");
+        console.log(error);
     } else {
         console.log("users table created");
     }
